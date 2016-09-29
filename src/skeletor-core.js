d198f80f39233cf3331f7bbdb3bbe7bb084357ab
Jeff <jmills@delphicdigital.com>
2016-09-29 11:30:10 -0400

    Get jquery working

--- src/skeletor-core.js
+++ src/skeletor-core.js
@@ -4,34 +4,54 @@ class Skeletor {
 	get VERSION() { return '0.3.0'; }
 
 	constructor(method){
-		let type = typeof method;
-
-		//console.log(type)
-
 		this._uuids = [];
-		//console.log('this is the skeletor constructor and will create the jquery plugin')
 	}
 
-	register(plugin){
-		//Get name of function
-		let name = plugin.name;
+	static jQueryPlugin(method){
+		let type = typeof method;
+		let pluginInstance = this.data('skeletorPlugin'); //determine the class of plugin
 
-		Object.defineProperty(Skeletor.prototype, name, {
+		if(type === 'undefined'){ //needs a parameter passed as a method
+			throw new ReferenceError(`We're sorry, you have to pass a method for ${(pluginInstance ? pluginInstance.NAME : 'this element')}.`);
+
+		}else if(type === 'string'){ //an individual method to invoke on a plugin or group of plugins
+			var args = Array.prototype.slice.call(arguments, 1);
+
+			//make sure both the class and method exist, skip private methods
+			if(pluginInstance !== undefined && method.charAt(0) !== '_' && pluginInstance[method] !== undefined){
+
+				if(this.length === 1){ //if there's only one, call it directly.
+					pluginInstance[method].apply(pluginInstance, args);
+
+				}else{
+
+					this.each(function(i, el){ //otherwise loop through the jQuery collection and invoke the method on each
+						pluginInstance[method].apply($(el).data('skeletorPlugin'), args);
+					});
+				}
+			}else{ //error for no class or no method
+				throw new ReferenceError("We're sorry, '" + method + "' is not an available method for " + (pluginInstance ? pluginInstance.NAME : 'this element') + '.');
+			}
+		}else{ //error for invalid argument type
+			throw new TypeError(`We're sorry, ${type} type is not a valid parameter. You must use a string representing the method you wish to invoke.`);
+		}
+
+		return this;
+	}
+
+	registerPlugin(plugin){
+
+		Object.defineProperty(Skeletor.prototype, plugin.name, {
 			get: function() {
 				return plugin;
 			}
 		})
 
-		console.info(`successfully registered skeletor plugin: ${name}`)
+		console.info(`successfully registered skeletor plugin: ${plugin.name}`)
 	}
 
-	set uuid(pluginName){
-		this._uuids.push(Skeletor.GetYoDigits(6, pluginName.toLowerCase()));
-	}
-
-	static GetYoDigits(length, namespace) {
-		length = length || 6;
-		return Math.round(Math.pow(36, length + 1) - Math.random() * Math.pow(36, length)).toString(36).slice(1) + (namespace ? '-' + namespace : '');
+	registerInstance(value){
+		this._uuids.push(value);
 	}
 
 }
@@ -39,4 +59,4 @@ class Skeletor {
 export let skeletor = new Skeletor();
 
 window.Skeletor = skeletor;
-
+$.fn.skeletor = Skeletor.jQueryPlugin;
