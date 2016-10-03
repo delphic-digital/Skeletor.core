1feac55e0bc17d0741d38e46ec2fda286c0f3af9
Jeff <jmills@delphicdigital.com>
2016-10-03 11:59:41 -0400

    Better

--- src/skeletor.js
+++ src/skeletor.js
@@ -58,7 +58,37 @@ class Skeletor {
 
 let skeletor = new Skeletor();
 
-window.Skeletor = skeletor;
-$.fn.skeletor = Skeletor.jQueryPlugin;
+class SkeletorPlugin {
 
-export default skeletor;
+	constructor(element, options){
+		this.NAME = this.constructor.name;
+		this.UUID = SkeletorPlugin.getYoDigits(6, this.constructor.name)
+		this.$element = element || $(document);
+		this.options = options;
+
+		//Store plugin on element for later retrievel
+		if(!this.$element.data('skeletorPlugin')){ this.$element.data('skeletorPlugin', this); }
+
+		console.log(skeletor)
+
+		//Store UUID with Skeletor
+		skeletor.registerInstance(this.UUID);
+	}
+
+	static register(){
+		skeletor.registerPlugin(this);
+	}
+
+	static getYoDigits(length, namespace) {
+		length = length || 6;
+		return Math.round(Math.pow(36, length + 1) - Math.random() * Math.pow(36, length)).toString(36).slice(1) + (namespace ? '-' + namespace : '');
+	}
+
+	set defaults(value){ this.options = $.extend(true, {}, value, this.options); }
+}
+
+
+window.Skeletor = skeletor;
+$.fn.skeletor = skeletor.jQueryPlugin;
+
+export { skeletor, SkeletorPlugin}
