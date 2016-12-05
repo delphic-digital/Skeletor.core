54eacdcfaeacfcfcb017fad22b7e5bc2381ccb2a
Jeff <jmills@delphicdigital.com>
2016-12-05 15:32:56 -0500

    Support custom elements

--- src/skeletor.core.js
+++ src/skeletor.core.js
@@ -1,15 +1,19 @@
 import $ from 'jquery';
 
 class Skeletor {
+	//TODO: Document skeletor core methods
+
 	get VERSION() { return '0.3.0'; }
 
 	constructor(method){
 		this._uuids = [];
+
+		this.setSelector();
 	}
 
-	static jQueryPlugin(method){
+	jQueryPlugin(method){
 		let type = typeof method;
-		let pluginInstance = this.data('skeletorPlugin'); //determine the class of plugin
+		let pluginInstance = this.data('skeletorComponent'); //determine the class of plugin
 
 		if(type === 'undefined'){ //needs a parameter passed as a method
 			throw new ReferenceError(`We're sorry, you have to pass a method for ${(pluginInstance ? pluginInstance.NAME : 'this element')}.`);
@@ -26,11 +30,11 @@ class Skeletor {
 				}else{
 
 					this.each(function(i, el){ //otherwise loop through the jQuery collection and invoke the method on each
-						pluginInstance[method].apply($(el).data('skeletorPlugin'), args);
+						pluginInstance[method].apply($(el).data('skeletorComponent'), args);
 					});
 				}
 			}else{ //error for no class or no method
-				throw new ReferenceError("We're sorry, '" + method + "' is not an available method for " + (pluginInstance ? pluginInstance.NAME : 'this element') + '.');
+				throw new ReferenceError(`We're sorry, ${ method} is not an available method for ${(pluginInstance ? pluginInstance.NAME : 'this element')}.`);
 			}
 		}else{ //error for invalid argument type
 			throw new TypeError(`We're sorry, ${type} type is not a valid parameter. You must use a string representing the method you wish to invoke.`);
@@ -39,7 +43,7 @@ class Skeletor {
 		return this;
 	}
 
-	registerPlugin(plugin){
+	registerComponent(plugin, elementName){
 
 		Object.defineProperty(Skeletor.prototype, plugin.name, {
 			get: function() {
@@ -47,46 +51,35 @@ class Skeletor {
 			}
 		})
 
+		window.customElements.define(elementName, plugin);
+
 		console.info(`successfully registered skeletor plugin: ${plugin.name}`)
 	}
 
-	registerInstance(value){
+	registerComponentInstance(value){
 		this._uuids.push(value);
+	}
+
+	getYoDigits(length, namespace) {
+		length = length || 6;
+		return Math.round(Math.pow(36, length + 1) - Math.random() * Math.pow(36, length)).toString(36).slice(1) + (namespace ? '-' + namespace : '');
+	}
+
+	setSelector(){
+		//Register a custom jquery selector for skeletor-* elements
+		$.extend($.expr[':'], {
+			skeletor : function(e) {
+				return /^skeletor-/i.test(e.tagName);
+			}
+		});
 	}
 
 }
 
 let skeletor = new Skeletor();
 
-class SkeletorPlugin {
-
-	constructor(element, options){
-		this.NAME = this.constructor.name;
-		this.UUID = SkeletorPlugin.getYoDigits(6, this.constructor.name)
-		this.$element = element || $(document);
-		this.options = options;
-
-		//Store plugin on element for later retrievel
-		if(!this.$element.data('skeletorPlugin')){ this.$element.data('skeletorPlugin', this); }
-
-		//Store UUID with Skeletor
-		skeletor.registerInstance(this.UUID);
-	}
-
-	static register(){
-		skeletor.registerPlugin(this);
-	}
-
-	static getYoDigits(length, namespace) {
-		length = length || 6;
-		return Math.round(Math.pow(36, length + 1) - Math.random() * Math.pow(36, length)).toString(36).slice(1) + (namespace ? '-' + namespace : '');
-	}
-
-	set defaults(value){ this.options = $.extend(true, {}, value, this.options); }
-}
-
-
 window.Skeletor = skeletor;
+
 $.fn.skeletor = skeletor.jQueryPlugin;
 
-export { skeletor, SkeletorPlugin}
+export default skeletor;
